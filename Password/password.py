26f5fc4fd6b54c17ff73edf109a5a14edba466bf
makenzie-ops <laurenmakenzie0@gmail.com>
2020-04-21 09:11:31 +0300

    first commit

---
+++ Password/password.py
@@ -0,0 +1,12 @@
+class password :
+    password_list = []
+
+
+    def __init__(self,first_name,last_name,login_username,login_password,app_name,username,acc_password):
+
+      # docstring removed for simplicity
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone_number = number
+        self.email = email
